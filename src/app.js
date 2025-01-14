a77b935ab254f92d80e5daa8f4abf2412b6c0252
Pablo Méndez <amandrangopablo@gmail.com>
2025-01-14 12:26:30 -0500

    refactor: :art: Refactor code, split server file in routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import express from 'express'
+import router from './routes/hello.routes.js'
 
 const app = express()
 const PORT = 3000 || process.env.PORT
 
-app.get("/", (req, res)=>{
-    res.send("Hello world")
-})
+app.use(router)
 
 app.listen(PORT, ()=>{
     console.log(`Server on port ${PORT}`)
